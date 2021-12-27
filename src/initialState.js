8380f2d764eda9a85cb129a6adf98d958517b38d
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-12-27 12:11:50 -0300

    Initial State

---
+++ src/initialState.js
@@ -0,0 +1,55 @@
+export default {
+	cart: [],
+	buyer: [],
+	orders: [],
+	products: [
+		{
+			id: '1',
+			image:
+				'https://s3.amazonaws.com/saved-drafts-production/saved_draft/11277649/image.png',
+			title: 'Camiseta',
+			price: 15,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+		},
+		{
+			id: '3',
+			image:
+				'https://s3.amazonaws.com/saved-drafts-production/saved_draft/11277678/image.png',
+			title: 'Taza',
+			price: 5,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+		},
+		{
+			id: '4',
+			image:
+				'https://s3.amazonaws.com/saved-drafts-production/saved_draft/11277715/image.png',
+			title: 'Gorro',
+			price: 13,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+		},
+		{
+			id: '5',
+			image:
+				'https://s3.amazonaws.com/saved-drafts-production/saved_draft/11277749/image.png',
+			title: 'Mochila',
+			price: 25,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+		},
+		{
+			id: '6',
+			image:
+				'https://s3.amazonaws.com/saved-drafts-production/saved_draft/11277801/image.png',
+			title: 'Pantalones',
+			price: 33,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+		},
+		{
+			id: '7',
+			image:
+				'https://s3.amazonaws.com/saved-drafts-production/saved_draft/11277817/image.png',
+			title: 'Buzo',
+			price: 55,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+		},
+	],
+}
